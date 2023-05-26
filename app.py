b19a170608da9e76c74ee469be29d14165192def
Salman Sakib <salmansrizon2016@gmail.com>
2023-05-26 15:20:33 +0000

    App.py working

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ application = Flask(__name__)
 
 app = application
 
-# Route for the home page
+# Route for a home page
 
 
 @app.route('/')
-def inde():
+def index():
     return render_template('index.html')
 
 
@@ -24,24 +24,25 @@ def predict_datapoint():
     else:
         data = CustomData(
             gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('race_ethnicity'),
+            race_ethnicity=request.form.get('ethnicity'),
             parental_level_of_education=request.form.get(
                 'parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get(
                 'test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score'))
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
+
         )
         pred_df = data.get_data_as_data_frame()
         print(pred_df)
         print("Before Prediction")
 
         predict_pipeline = PredictPipeline()
-        print("Mid Prediction ")
-        result = predict_pipeline.predict(pred_df)
-        print("After Prediction")
-        return render_template('home.html', result=result[0])
+        print("Mid Prediction")
+        results = predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html', results=results[0])
 
 
 if __name__ == "__main__":
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,5 +1,5 @@
 import sys
-import os
+import os 
 import pandas as pd
 from src.exception import CustomException
 from src.utlis import load_object
@@ -12,7 +12,7 @@ class PredictPipeline:
     def predict(self, features):
         try:
             model_path = os.path.join("artifacts", "model.pkl")
-            preprocessor_path = os.path.join("artifacts", "proprocessor.pkl")
+            preprocessor_path = os.path.join('artifacts', 'proprocessor.pkl')
             print("Before Loading")
             model = load_object(file_path=model_path)
             preprocessor = load_object(file_path=preprocessor_path)
@@ -20,6 +20,7 @@ class PredictPipeline:
             data_scaled = preprocessor.transform(features)
             preds = model.predict(data_scaled)
             return preds
+
         except Exception as e:
             raise CustomException(e, sys)
 
@@ -32,15 +33,20 @@ class CustomData:
                  lunch: str,
                  test_preparation_course: str,
                  reading_score: int,
-                 writing_score: int
-                 ):
+                 writing_score: int):
 
         self.gender = gender
+
         self.race_ethnicity = race_ethnicity
+
         self.parental_level_of_education = parental_level_of_education
+
         self.lunch = lunch
+
         self.test_preparation_course = test_preparation_course
+
         self.reading_score = reading_score
+
         self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
@@ -54,6 +60,7 @@ class CustomData:
                 "reading_score": [self.reading_score],
                 "writing_score": [self.writing_score],
             }
+
             return pd.DataFrame(custom_data_input_dict)
 
         except Exception as e:
